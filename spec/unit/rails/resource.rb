19d58d78d972f2249d71c41bc87183a47cc8f83f
Luke Kanies <luke@madstop.com>
2009-08-01 16:49:03 -0700

    Always providing a value for 'exported' on Rails resources
    We often didn't set a value, unless it was true, which
    meant that if it had previously been true but was now
    false, we didn't fix it.
    
    We also were not always saving modified resources, which
    in some cases resulted in work not getting saved.
    
    Signed-off-by: Luke Kanies <luke@madstop.com>
    
    Conflicts:
    
    	spec/unit/rails/resource.rb

---
+++ spec/unit/rails/resource.rb
@@ -0,0 +1,87 @@
+#!/usr/bin/env ruby
+
+require File.dirname(__FILE__) + '/../../spec_helper'
+
+describe "Puppet::Rails::Resource" do
+    confine "Cannot test without ActiveRecord" => Puppet.features.rails?
+
+    def column(name, type)
+        ActiveRecord::ConnectionAdapters::Column.new(name, nil, type, false)
+    end
+
+    before do
+        require 'puppet/rails/resource'
+
+        # Stub this so we don't need access to the DB.
+        Puppet::Rails::Resource.stubs(:columns).returns([column("title", "string"), column("restype", "string"), column("exported", "boolean")])
+    end
+
+    describe "when creating initial resource arguments" do
+        it "should set the restype to the resource's type" do
+            Puppet::Rails::Resource.rails_resource_initial_args(Puppet::Resource.new(:file, "/file"))[:restype].should == "File"
+        end
+
+        it "should set the title to the resource's title" do
+            Puppet::Rails::Resource.rails_resource_initial_args(Puppet::Resource.new(:file, "/file"))[:title].should == "/file"
+        end
+
+        it "should set the line to the resource's line if one is available" do
+            resource = Puppet::Resource.new(:file, "/file")
+            resource.line = 50
+
+            Puppet::Rails::Resource.rails_resource_initial_args(resource)[:line].should == 50
+        end
+
+        it "should set 'exported' to true of the resource is exported" do
+            resource = Puppet::Resource.new(:file, "/file")
+            resource.exported = true
+
+            Puppet::Rails::Resource.rails_resource_initial_args(resource)[:exported].should be_true
+        end
+
+        it "should set 'exported' to false of the resource is not exported" do
+            resource = Puppet::Resource.new(:file, "/file")
+            resource.exported = false
+
+            Puppet::Rails::Resource.rails_resource_initial_args(resource)[:exported].should be_false
+
+            resource = Puppet::Resource.new(:file, "/file")
+            resource.exported = nil
+
+            Puppet::Rails::Resource.rails_resource_initial_args(resource)[:exported].should be_false
+        end
+    end
+
+    describe "when merging in a parser resource" do
+        before do
+            @parser = mock 'parser resource'
+
+            @resource = Puppet::Rails::Resource.new
+            [:merge_attributes, :merge_parameters, :merge_tags, :save].each { |m| @resource.stubs(m) }
+        end
+
+        it "should merge the attributes" do
+            @resource.expects(:merge_attributes).with(@parser)
+
+            @resource.merge_parser_resource(@parser)
+        end
+
+        it "should merge the parameters" do
+            @resource.expects(:merge_parameters).with(@parser)
+
+            @resource.merge_parser_resource(@parser)
+        end
+
+        it "should merge the tags" do
+            @resource.expects(:merge_tags).with(@parser)
+
+            @resource.merge_parser_resource(@parser)
+        end
+
+        it "should save itself" do
+            @resource.expects(:save)
+
+            @resource.merge_parser_resource(@parser)
+        end
+    end
+end
